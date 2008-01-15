ba31d5122bcc9147b6ecea781714c52a04d79e06
mikesamuel <mikesamuel@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-01-15 04:07:52 +0000

    A demo application for the hackathon on Wednesday.
    The app demos 2 cooparating gadgets
    
    (1) The searchbox gadget displays a search box and uses an
       injected search engine capability to fetch search
       results which it displays in a result list.  It has been
       injected with a function from the second gadget which it
       invokes when a result is clicked.
    
    (2) The kittens gadget waits for a result to be clicked, and
       then does an image search for "cute cats <result title>"
       and displays one of the images, along with the result
       snippet translated into LOLCat.
    
    
    This demonstrates:
    (1) Mutually suspicious yet cooperating gadgets.
    (2) Namespacing of styles.
    (3) Statically verified HTML with handler extraction.
    (4) Tamed dom manipulation and event handling.
    (5) Injection of capabilities.
    
    
    
    
    See the demo under java/com/google/caja/demos/lolcat-search:
     search.html -- The container page.
    
     searchbox.html -- The searchbox gadget.
     searchbox.{js,css} -- loaded via script and link tags.
    
     kittens.html -- The kittens gadget.
     kittens.js -- code for kittens gadget
     katTranzlator.js -- sophisticated machine translation in JS
     loading.jpg, error.jpg -- images used by kittens
    
     container.js -- A bare-bones tamed DOM, and implementations
                     of emitHtml___ & plugin_dispatchEvent___
     chtml.js -- A copy of open(Template(...)) used by gadgets
     searchengine.js -- An interface to Google AJAX Search APIs
    
    
    
    Also in this change:
    - Fixed bug: MultipleQuasiHole was cloning children
     and so losing the synthetic attribute.
     This manifested when an event handler referred to this.
     All references to this are rewritten to thisNode___, and
     when that was used in a function calls list of actuals,
     the MultipleQuasiHole cloned the thisNode___ reference
     but without making it synthetic, and the clone was then
     processed by another rule which broke it.
    
    - Added returns toto several rewrite rules.  Previously
     the values were being dropped on the floor.
    
    - Added rewrite rules for { for(k in v) } rules.
     I added one rule for the case where k is declared, and
     one where it's a reference declared elsewhere.  The
     rules look like:
             for (var @k in @obj) @body
         =>
             {(function () {
               var o___ = @obj;
               for (var @k in o___) {
                 if (!___.canRead(o___, @k)) continue;
                 @body;
               }
             })();}
    
    - Added ArrayConstructor and MultiDeclaration to the list
     of nodes that can be cajoled.
    
    - Fixed the order of parameters passed to the
     plugin_dispatchEvent___ function to match that in the
     documentation.
    
    - Added a pipeline stage to optimize open(Template(...))
     calls.
    
    - Fixed error message in RewriteHtmlStage.
    
    - Rewrote HtmlPluginCompilerMain to load external scripts
     and stylesheets from the local filesystem.
    
    - Added a build rule 'lolcat-search' that builds the demo
     and dumps it under ant-www/lolcat-search.

---
+++ changes/mikesamuel/hackathon_demo_gadgets_12_Jan_2008/trunk/src/NOT-FOR-TRUNK/lolcat-search/kittens-cajoled.js
@@ -0,0 +1,95 @@
+{
+  ___.loadModule(function (___OUTERS___) {
+      ___OUTERS___.katTranzlator = ___.simpleFunc(function (text) {
+          text = (function () {
+              var x___ = (function () {
+                  var x___ = (function () {
+                      var x___ = text;
+                      return x___.toUpperCase_canCall___ ? x___.toUpperCase() : ___.callPub(x___, 'toUpperCase', []);
+                    })();
+                  var x0___ = /\s+/g;
+                  var x1___ = ' ';
+                  return x___.replace_canCall___ ? x___.replace(x0___, x1___) : ___.callPub(x___, 'replace', [x0___, x1___]);
+                })();
+              var x0___ = /^ +| +$/g;
+              var x1___ = '';
+              return x___.replace_canCall___ ? x___.replace(x0___, x1___) : ___.callPub(x___, 'replace', [x0___, x1___]);
+            })();
+          for (var i = 0, n = (function () {
+                  var x___ = ___OUTERS___.TRANSFORMATIONS;
+                  return x___.length_canRead___ ? x___.length : ___.readPub(x___, 'length');
+                })(); i < n; i += 2) {
+            text = (function () {
+                var x___ = text;
+                var x0___ = ___.readPub(___OUTERS___.TRANSFORMATIONS, i);
+                var x1___ = ___.readPub(___OUTERS___.TRANSFORMATIONS, i + 1);
+                return x___.replace_canCall___ ? x___.replace(x0___, x1___) : ___.callPub(x___, 'replace', [x0___, x1___]);
+              })();
+          }
+          return text;
+        });
+      ___OUTERS___.TRANSFORMATIONS = [/\bCAN (I|YOU|HE|SHE|IT|WE|THEY)\b/g, '$1 CAN', /\bTO\b/g, '2', /\bFOR\b/g, '4', /\bYOUR\b/g, 'UR', /\bYOU\b/g, 'U', /\bTHIS\b/g, 'DIS', /\bWITH\b/g, 'WIF', /\bHAVE\b/g, 'HAZ', /\bARE\b/g, 'IS', /\bAM\b/g, 'IS', /\bPLEASE\b/g, 'PLZ', /\bTHANKS\b/g, 'THX', /\bOH MY (GOD|GOSH)\b/g, 'OMG', /\bATE\b/g, 'EATED', /\bSAID\b/g, 'SED', /\bSERIOUSLY\b/g, 'SRSLY', /\bKNOW\b/g, 'KNOE', /\bLOVE\b/g, 'LUV', /\bHELP\b/g, 'HALP', /\bMAYBE\b/g, 'MEBBE', /\bWAS\b/g, 'WUZ', /\bOF\b/g, 'OV', /\bOH\b/g, 'O', /\bREALLY\b/g, 'RLY', /\bGREAT\b/g, 'GRAET', /\bMY\b/g, 'MAH', /\b(HELLO|HI)\b/g, 'HAI', /THI/g, 'TI', /\bKN/g, 'N', /([^ ])SE(\b|[^AEIOU])/g, '$1ZE$2', /IES\b/g, 'EHS', /TION(S?)\b/g, 'SHUN', /LE\b/g, 'L', /IENDS\b/g, 'ENZ', /([^R])ING\b/g, '$1IN', /I([KM])E\b/g, 'IE$1', /ER( [^AEIOU]|$)/g, 'AH$1', /ORE\b/g, 'OAR', /IE\b/g, 'EE', /AIR\b/g, 'EH', /IEF\b/g, 'EEF', /TY\b/g, 'TI', /NESS\b/g, 'NES', /([^ AEIOU])E([RD])\b/g, '$1$2', /IC\b/g, 'IK', /VE\b/g, 'V', /FORE\b/g, 'FOA', /(O[^ AEIOU])E\b/g, '$1', /\bPH([AEIOU])/g, 'F$1', /([^AEIOU ])IR/g, '$1UR', /([^AEIOU ])S\b/g, '$1Z', /([^ AEIOU]) OV\b/g, '$1A', /N\'T/g, 'NT', /OAR/g, 'OR', /IGHT/g, 'ITE', /([AEIOU])S([BDFJV])/g, '$1Z$2', /CEIV/g, 'CEEV', /AUGHT/g, 'AWT', /OO/g, 'U', /U([^ AEIOU])E/g, 'OO$1', /U([^ AEIOU]I)/g, 'OO$1', /CIOUS/g, 'SHUS', /OUCH/g, 'OWCH', /ISON/g, 'ISUN', /OIS/g, 'OYZ', /\bSEAR/g, 'SER', /\bSEA/g, 'SEE', /\bGOD/g, 'CEILING CAT', /\bHEAVEN/g, 'CEILING', /([AEIOU])[SZ]E/g, '$1Z', /\bI AM\b/g, 'I', /\bIZ A\b/g, 'IS', /\bHAZ NO\b/g, 'NO HAZ', /\bDO YOU\b/g, 'YOU', /\bA ([A-Z]+)\b/g, '$1', /\bI IS\b/g, 'IM'];
+      ___OUTERS___.emitHtml___('\n');
+      var searchEngine;
+      ___OUTERS___.showKitten = ___.simpleFunc(function (result) {
+          var title = (function () {
+              var x___ = (function () {
+                  var x___ = result;
+                  return x___.titleHtml_canRead___ ? x___.titleHtml : ___.readPub(x___, 'titleHtml');
+                })();
+              var x0___ = /\074\/?\w[^\076]*\076/g;
+              var x1___ = '';
+              return x___.replace_canCall___ ? x___.replace(x0___, x1___) : ___.callPub(x___, 'replace', [x0___, x1___]);
+            })();
+          var snippet = (function () {
+              var x___ = (function () {
+                  var x___ = result;
+                  return x___.snippetHtml_canRead___ ? x___.snippetHtml : ___.readPub(x___, 'snippetHtml');
+                })();
+              var x0___ = /\074\/?\w[^\076]*\076/g;
+              var x1___ = '';
+              return x___.replace_canCall___ ? x___.replace(x0___, x1___) : ___.callPub(x___, 'replace', [x0___, x1___]);
+            })();
+          ___.asSimpleFunc(___.primFreeze(___OUTERS___.renderKittenTable))('loading.jpg', snippet);
+          (function () {
+              var x___ = ___OUTERS___.searchEngine;
+              var x0___ = 'cute (kitten OR cat) ' + title;
+              var x1___ = ___.primFreeze(___.simpleFunc(function (imageResults) {
+                    var n = (function () {
+                        var x___ = imageResults;
+                        return x___.length_canRead___ ? x___.length : ___.readPub(x___, 'length');
+                      })();
+                    if (!n) {
+                      ___.asSimpleFunc(___.primFreeze(___OUTERS___.renderKittenTable))('error.jpg', snippet);
+                      return undefined;
+                    }
+                    var k = (function () {
+                        var x___ = ___OUTERS___.Math;
+                        return x___.random_canCall___ ? x___.random() : ___.callPub(x___, 'random', []);
+                      })() * n | 0;
+                    ___.asSimpleFunc(___OUTERS___.log)('chose ' + k + ' from ' + (function () {
+                          var x___ = imageResults;
+                          return x___.length_canRead___ ? x___.length : ___.readPub(x___, 'length');
+                        })());
+                    ___.asSimpleFunc(___.primFreeze(___OUTERS___.renderKittenTable))((function () {
+                          var x___ = ___.readPub(imageResults, k);
+                          return x___.url_canRead___ ? x___.url : ___.readPub(x___, 'url');
+                        })(), snippet);
+                  }));
+              return x___.imageSearch_canCall___ ? x___.imageSearch(x0___, x1___) : ___.callPub(x___, 'imageSearch', [x0___, x1___]);
+            })();
+        });
+      ___OUTERS___.renderKittenTable = ___.simpleFunc(function (imageUrl, snippet) {
+          (function () {
+              var x___ = (function () {
+                  var x___ = ___OUTERS___.document;
+                  var x0___ = 'base';
+                  return x___.getElementById_canCall___ ? x___.getElementById(x0___) : ___.callPub(x___, 'getElementById', [x0___]);
+                })();
+              var x0___ = new (___.asCtor(___OUTERS___.StringInterpolation))(['\074table\076\074tr\076\074td align=center\076\074img src=\"', imageUrl, '\"\076\074/tr\076\074tr\076\074td style=width:30em align=center\076', ___.asSimpleFunc(___.primFreeze(___OUTERS___.katTranzlator))(snippet || ''), '\074/table\076']);
+              return x___.setInnerHTML_canCall___ ? x___.setInnerHTML(x0___) : ___.callPub(x___, 'setInnerHTML', [x0___]);
+            })();
+        });
+      ___OUTERS___.emitHtml___('\n');
+    });
+}
---
+++ changes/mikesamuel/hackathon_demo_gadgets_12_Jan_2008/trunk/src/java/com/google/caja/parser/quasiliteral/Scope.java
@@ -0,0 +1,279 @@
+// Copyright (C) 2007 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.caja.parser.quasiliteral;
+
+import com.google.caja.parser.AncestorChain;
+import com.google.caja.parser.ParseTreeNode;
+import com.google.caja.parser.Visitor;
+import com.google.caja.parser.js.Block;
+import com.google.caja.parser.js.Declaration;
+import com.google.caja.parser.js.FunctionConstructor;
+import com.google.caja.parser.js.FunctionDeclaration;
+import com.google.caja.parser.js.Reference;
+import com.google.caja.plugin.ReservedNames;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * A scope analysis of a {@link com.google.caja.parser.ParseTreeNode}.
+ *
+ * @author ihab.awad@gmail.com (Ihab Awad)
+ */
+public class Scope {
+  private enum LocalType {
+    /**
+     * A named function value, visible only within its own body.
+     * Examples: "foo" in the following --
+     *
+     * <pre>
+     * var y = function foo() { };
+     * zip(function foo() { });
+     * </pre>
+     */
+    FUNCTION(),
+
+    /**
+     * A function declaration, visible in its enclosing scope.
+     * Example: "foo" in the following --
+     *
+     * <pre>
+     * function foo() { }
+     * </pre>
+     */
+    DECLARED_FUNCTION(FUNCTION),
+
+    /**
+     * A constructor declaration, i.e., one which mentions 'this' in its body,
+     * Example: "foo" in the following --
+     *
+     * <pre>
+     * function foo() { this.x = 3; }
+     * </pre>
+     */
+    CONSTRUCTOR(DECLARED_FUNCTION),
+
+    /**
+     * A variable containing arbitrary data (including functions).
+     * Examples: "x", "y", "z" and "t" in the following --
+     *
+     * <pre>
+     * var x;
+     * var y = 3;
+     * var z = function() { };
+     * var t = function foo() { };
+     * </pre>
+     */
+    DATA();
+
+    private final HashSet<LocalType> implications;
+
+    private LocalType(LocalType... implications) {
+      this.implications = new HashSet<LocalType>();
+      this.implications.addAll(Arrays.asList(implications));
+    }
+
+    public boolean implies(LocalType type) {
+      if (this == type) return true;
+      for (LocalType i : implications) {
+        if (i.implies(type)) return true;
+      }
+      return false;
+    }
+  }
+  
+  private Scope parent;
+  private boolean containsThis;
+  private boolean containsArguments;
+  private Map<String, LocalType> locals = new HashMap<String, LocalType>();
+
+  // TODO(ihab.awad): Create scope from static methods.
+  // TODO(ihab.awad): Take a message queue for adding error messages.
+
+  /**
+   * Create a scope from some arbitrary parse tree nodes.
+   *
+   * @param parent the parent scope.
+   * @param root the node to use as a root.
+   */
+  public Scope(Scope parent, ParseTreeNode root) {
+    this.parent = parent;
+    walkBlock(root);
+  }
+
+
+  /**
+   * Create a top-level scope for a program.
+   *
+   * @param root the block representing the top level of the program.
+   */
+  public Scope(Block root) {
+    walkBlock(root);
+  }
+
+  /**
+   * Create a nested scope for a function in a program.
+   *
+   * @param parent the parent scope of the function.
+   * @param root the function constituting the nested scope.
+   */
+  public Scope(Scope parent, FunctionConstructor root) {
+    this.parent = parent;
+
+    if (root.getIdentifierName() != null && parent != null) {
+      LocalType defType = parent.getType(root.getIdentifierName());
+      if (defType == null || !defType.implies(LocalType.FUNCTION)) {
+        locals.put(root.getIdentifierName(), LocalType.FUNCTION);
+      }
+    }
+
+    for (ParseTreeNode n : root.getParams()) walkBlock(n);
+    walkBlock(root.getBody());
+  }
+
+  /**
+   * The parent of this scope.
+   *
+   * @return a {@code Scope} or {@code null}.
+   */
+  public Scope getParent() {
+    return parent;
+  }
+
+  /**
+   * Does this scope mention "this" freely?
+   *
+   * <p>If "this" is only mentioned within a function definition within
+   * this scope, then the result is <tt>false</tt>, since that "this"
+   * isn't a free occurrence.
+   *
+   * @return whether this block has a free "this".
+   */
+  public boolean hasFreeThis() {
+    return containsThis;
+  }
+
+  /**
+   * Does this scope mention "arguments" freely?
+   *
+   * <p>If "arguments" is only mentioned within a function definition
+   * within this scope, then the result is <tt>false</tt>, since that
+   * "arguments" isn't a free occurrence.
+   *
+   * @return whether this block has a free "arguments".
+   */
+  public boolean hasFreeArguments() {
+    return containsArguments;
+  }
+
+  /**
+   * Does this scope or some enclosing scope define a name?
+   *
+   * @param name an identifier.
+   * @return whether 'name' is defined within this scope.
+   */
+  public boolean isDefined(String name) {
+    return getType(name) != null;
+  }
+
+  /**
+   * In this scope or some enclosing scope, is a given name
+   * defined as a function?
+   *
+   * @param name an identifier.
+   * @return whether 'name' is defined as a function within this
+   * scope. If 'name' is not defined, return false.
+   */
+  public boolean isFunction(String name) {
+    return isDefined(name) && getType(name).implies(LocalType.FUNCTION);
+  }
+
+  /**
+   * In this scope or some enclosing scope, is a given name
+   * defined as a declared function?
+   *
+   * @param name an identifier.
+   * @return whether 'name' is defined as a declared function within this
+   * scope. If 'name' is not defined, return false.
+   */
+  public boolean isDeclaredFunction(String name) {
+    return isDefined(name) && getType(name).implies(LocalType.DECLARED_FUNCTION);
+  }
+
+  /**
+   * In this scope or some enclosing scope, is a given name defined
+   * as a constructor?
+   *
+   * @param name an identifier.
+   * @return whether 'name' is defined as a constructor within this
+   * scope. If 'name' is not defined, return false.
+   */
+  public boolean isConstructor(String name) {
+    return isDefined(name) && getType(name).implies(LocalType.CONSTRUCTOR);
+  }
+
+  /**
+   * Is a given symbol global?
+   *
+   * @param name an identifier.
+   * @return whether 'name' is a defined global variable.
+   */
+  public boolean isGlobal(String name) {
+    return
+        parent == null ||
+        (!locals.containsKey(name) && parent.isGlobal(name));
+  }
+
+  private LocalType getType(String name) {
+    return locals.containsKey(name) ?
+        locals.get(name) :
+        parent != null ? parent.getType(name) : null;
+  }
+
+  private LocalType computeDeclarationType(Declaration decl) {
+    if (decl instanceof FunctionDeclaration) {
+      Scope s2 = new Scope(this, ((FunctionDeclaration)decl).getInitializer());
+      return s2.hasFreeThis() ? LocalType.CONSTRUCTOR : LocalType.DECLARED_FUNCTION;
+    }
+    return LocalType.DATA;
+  }
+
+  private void walkBlock(ParseTreeNode root) {
+    root.acceptPreOrder(new Visitor() {
+      public boolean visit(AncestorChain<?> chain) {
+        if (chain.node instanceof FunctionConstructor) {
+          return false;
+        }
+        if (chain.node instanceof Declaration) {
+          LocalType type = computeDeclarationType((Declaration)chain.node);
+          String name = ((Declaration)chain.node).getIdentifierName();
+          if (locals.containsKey(name) && locals.get(name) != type) {
+            throw new RuntimeException("Duplicate definition of local: " + name);
+          }
+          locals.put(name, type);
+        }
+        if (chain.node instanceof Reference) {
+          String name = ((Reference)chain.node).getIdentifierName();
+          if (ReservedNames.ARGUMENTS.equals(name)) { containsArguments = true; }
+          if (ReservedNames.THIS.equals(name)) { containsThis = true; }
+        }
+        return true;
+      }
+    },
+    null);
+  }
+}
