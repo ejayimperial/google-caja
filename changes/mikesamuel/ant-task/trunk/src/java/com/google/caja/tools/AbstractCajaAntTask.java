4bbde5eff9ef114f062505cc1ac2d3e9c6ce0477
mikesamuel <mikesamuel@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-05-02 11:29:31 +0000

    Added custom ant tasks to make it easy to generate js for selenium, and ported old demos to caja.
    Ant Tasks
    =========
    The ant tasks invoke the cajoler & minifier to produce javascript from
    inputs in the source tree.  This is used by unittests which execute
    cajoled code, by demo code, and by code which packages up jars to
    include a single file for container writers to include.
    
    Previously the build file included <java> tasks, but these were hard
    to read and slowed down the build because they don't skip rebuilding
    when the output is older than the input.
    
    Files:
    BuildService -- an interface between the task, and PluginCompilerMain
    AbstractCajaAntTask.java -- collects input and output files from the
        build file, and determines whether or not we need to rebuild
    CajoleAntTask & MinifyAntTask -- concrete ant tasks
    
    
    Build File Cleanup
    ==================
    - Rewrote the demo, minify, and CajoledTests tasks to use the new ANT
      tasks.
    - Collapsed unnecessary intermediate tasks which speeds up builds
      somewhat.
    - Simplified classpath definitions
    
    
    Vestigial AAJA Code
    ===================
    Got rid of plugin-base, wrap_capabilities, and related tests left over
    from AAJA style capabilities.
    
    
    SpringGraph Demo
    ================
    Got rid of the old capability wrapping code from AAJA, and reworked
    the demo to work with DOMita.
    
    Fixed a slew of DOMita bugs.
    - DOM nodes reached via nextSibling, prevSibling, firstChild not
      mutable.
    - Event dispatch not editable.
    - Setting of class name failed silently.
    - Wrong CSS prefix used.
    - GXP compiler failing to properly cajole event handlers.
    - Implemented getters for node positions: offsetTop,offsetWidth,...
    - Implemented setTimeout, setInterval, and the clearers.
    
    
    JavaScript Cleanup
    ==================
    - Got rid of tmp-caja.js, a copy of an old caja.js inexplicably in our
      java tree.
    - Got rid of JSON.js which was copied into our source tree sans
      license file, and which is buggy and has known exploits.  Added
      JSON2.js to third_party instead.
    - Modified Q-caja.js to use JSON2.

---
+++ changes/mikesamuel/ant-task/trunk/src/java/com/google/caja/tools/AbstractCajaAntTask.java
@@ -0,0 +1,197 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.caja.tools;
+
+import com.google.caja.plugin.BuildServiceImplementation;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * An ANT task that operates on a set of files to produce a single output file
+ * via a {@link BuildService}.
+ *
+ * @see <a href="http://ant.apache.org/manual/tutorial-writing-tasks.html">ANT
+ *     Task Tutorial</a>
+ * @see <a href="http://ant.apache.org/manual/CoreTasks/typedef.html">Deploting
+ *     Tasks</a>
+ *
+ * @author mikesamuel@gmail.com
+ */
+abstract class AbstractCajaAntTask extends Task {
+  /** A set of {@code <job>} tasks. */
+  private final List<Job> jobs = new ArrayList<Job>();
+
+  /** Called to actually execute a job by invoking the BuildService. */
+  protected abstract boolean run(BuildService buildService, PrintWriter logger,
+                                 File baseDir, List<File> inputs, File output,
+                                 Map<String, Object> options)
+      throws BuildException;
+
+  @Override
+  public void execute() throws BuildException {
+    if (jobs.isEmpty()) {
+      throw new BuildException("caja task must have one or more <job>s");
+    }
+    for (Job job : jobs) { job.requireExecutable(); }
+
+    BuildService buildService = getBuildService();
+    PrintWriter logger = getLogger();
+    try {
+      for (Job job : jobs) {
+        job.execute(buildService, logger);
+      }
+    } finally {
+      logger.flush();
+    }
+  }
+
+  /** Invoked reflectively by ANT when a <job> is seen. */
+  public Job createJob() {
+    Job job = new Job();
+    jobs.add(job);
+    return job;
+  }
+
+  /**
+   * Wrap {@link Task#log(String)} in a PrintWriter so BuildService doesn't have
+   * to know about ANT.
+   */
+  PrintWriter getLogger() {
+    return new PrintWriter(
+        new Writer() {
+          StringBuilder sb = new StringBuilder();
+          @Override
+          public void write(char[] cbuf, int off, int len) {
+            sb.append(cbuf, off, len);
+          }
+          @Override
+          public void flush() {
+            if (sb.length() != 0) {
+              log(sb.toString().trim());
+              sb = new StringBuilder();
+            }
+          }
+          @Override
+          public void close() {}
+        }, true);
+  }
+
+  BuildService getBuildService() throws BuildException {
+    return new BuildServiceImplementation();
+  }
+
+  /** Encapsulates input files and the output files that should be produced. */
+  public class Job {
+    /** The file to build. */
+    private File output;
+    /**
+     * The base source directory.  Any file references within source files must
+     * resolve under this directory.
+     */
+    private File srcDir;
+    /** Inputs files to compile. */
+    private final List<Include> includes = new ArrayList<Include>();
+
+    public Include createInclude() {
+      Include include = new Include();
+      includes.add(include);
+      return include;
+    }
+
+    /**
+     * The below are invoked reflectively by ant when {@code <job>} attributes
+     * are seen.
+     */
+    public void setOutput(File output) { this.output = output; }
+    public void setSrc(File srcDir) { this.srcDir = srcDir; }
+
+    /** Checks that there is enough information to execute before execution. */
+    void requireExecutable() throws BuildException {
+      if (output == null) {
+        throw new BuildException(
+            "<job> at " + getLocation() + "missing 'output' attribute");
+      }
+      if (srcDir != null && !srcDir.isDirectory()) {
+        throw new BuildException(
+            "src '" + srcDir + "' at " + getLocation() + "is not a directory");
+      }
+      if (includes.isEmpty()) {
+        throw new BuildException(
+            "<job> at " + getLocation() + "missing <include>s");
+      }
+      for (Include include : includes) {
+        include.requireExecutable();
+      }
+    }
+
+    /** Builds output, throwing a BuildException if build fails. */
+    void execute(BuildService buildService, PrintWriter logger)
+        throws BuildException {
+      long outputModified = output.lastModified();
+      boolean modified = false;  // -> the output file is older than any inputs.
+      if (outputModified == 0L) { modified = true; }  // 0L -> !output.exists()
+
+      // Make sure the output directory exists.
+      File outputDir = output.getParentFile();
+      if (!outputDir.exists()) {
+        logger.println("mkdir " + outputDir);
+        outputDir.mkdirs();
+      }
+
+      List<File> inputs = new ArrayList<File>();
+      for (Include include : includes) {
+        if (!modified && include.file.lastModified() > outputModified) {
+          modified = true;
+        }
+        inputs.add(include.file);
+      }
+      if (modified) {
+        logger.println("compiling " + inputs.size() + " files to " + output);
+        File baseDir = srcDir != null ? srcDir : getProject().getBaseDir();
+        if (!run(buildService, logger, baseDir, inputs, output,
+                 Collections.<String, Object>emptyMap())) {
+          if (output.exists()) { output.delete(); }
+          throw new BuildException("Failed to build " + output);
+        }
+      }
+    }
+  }
+
+  /** An input file. */
+  public class Include {
+    /** An input file.  Could use FileSet to get globbing? */
+    private File file;
+
+    public void setFile(File file) { this.file = file; }
+
+    void requireExecutable() throws BuildException {
+      if (file == null) {
+        throw new BuildException("<include> at " + getLocation()
+                                 + "missing 'file' attribute");
+      }
+      if (!file.canRead()) {
+        throw new BuildException("<include> at " + getLocation()
+                                 + "of '" + file + "' is not readable");
+      }
+    }
+  }
+}
