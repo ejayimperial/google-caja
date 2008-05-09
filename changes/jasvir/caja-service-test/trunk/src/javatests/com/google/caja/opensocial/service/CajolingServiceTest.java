7a19fddbd9aaeef7f3e075d324271308e3e502c8
jasvir <jasvir@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-05-09 00:52:46 +0000

    Fixes a flushing bug in the cajoling service.
    * Added a flush after writing to BufferedWriter before accessing the InputStream that it is wrapped around
    * Added a test case for checking the cajoling using the service.
    * Added missing exception class UnsupportedEncodingException

--- changes/jasvir/caja-service-test/trunk/src/javatests/com/google/caja/opensocial/service/CajolingServiceTest.java
+++ changes/jasvir/caja-service-test/trunk/src/javatests/com/google/caja/opensocial/service/CajolingServiceTest.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2007 Google Inc.
+// Copyright (C) 2008 Google Inc.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -15,6 +15,8 @@ package com.google.caja.opensocial.service;
 
 import com.google.caja.lexer.ParseException;
 import com.google.caja.parser.ParseTreeNodes;
+import com.google.caja.parser.quasiliteral.DefaultCajaRewriter;
+import com.google.caja.parser.quasiliteral.DefaultCajaRewriterTest;
 import com.google.caja.util.CajaTestCase;
 
 import com.sun.net.httpserver.HttpContext;
@@ -38,31 +40,41 @@ import java.net.URLEncoder;
  * @author jasvir@google.com (Jasvir Nagra)
  */
 public class CajolingServiceTest extends CajaTestCase {
+  
+  // Port on which the test generating server listens
+  // and returns test snippets for cajoling
+  final private int HTTP_TEST_PORT = 18887;
+  
+  // Port on which the cajoling service runs
+  // cajols urls which are requested via it
+  final private int CAJOLING_SERVICE_PORT = 8887;
+  
   private CajolingService service;
+  private HttpServer httpServer;
   private TestingHttpHandler httpService;
   
-  
-  /**
-   * Creates a webservice which returns the 
-   */
   private class TestingHttpHandler implements HttpHandler {
   private String testInstance;
   private String contentType;
+  private String charSet;
   
-  public void setTest(String test, String contentType) {
+  public void setTest(String test, String contentType, String charSet) {
     this.testInstance = test;
     this.contentType = contentType;
+    this.charSet = charSet;
   }
 
-   // returns 
    public void handle(HttpExchange ex) throws IOException {
      Reader request = new InputStreamReader(ex.getRequestBody());
      ex.getResponseHeaders().set("Content-Type", contentType);
      ex.sendResponseHeaders(HttpStatus.OK.value(), 0);
-     Writer response = new OutputStreamWriter(ex.getResponseBody());
-     response.write(testInstance);
-     System.out.println(testInstance);
-     response.close();
+     Writer response = null;
+     try {
+       response = new OutputStreamWriter(ex.getResponseBody(), charSet);
+       response.write(testInstance);
+     } finally {
+       response.close();
+     }
    }
   }
   
@@ -70,17 +82,19 @@ public class CajolingServiceTest extends CajaTestCase {
   public void setUp() throws Exception {
     super.setUp();
     service = new CajolingService();
-    HttpServer server = HttpServer.create(new InetSocketAddress(18887),0);
+    httpServer = HttpServer.create(new InetSocketAddress(HTTP_TEST_PORT),0);
     httpService = new TestingHttpHandler();
-    HttpContext ctx = server.createContext("/cajaservicetest", httpService);
-    server.setExecutor(null);
-    server.start();
+    HttpContext ctx = httpServer.createContext("/cajaservicetest", httpService);
+    httpServer.setExecutor(null);
+    httpServer.start();
     service.start();
   }
 
   @Override
   public void tearDown() { 
     service.stop();
+    // Stop the http server immediately
+    httpServer.stop(0);
     service = null; 
     httpService = null;
   }
@@ -88,21 +102,20 @@ public class CajolingServiceTest extends CajaTestCase {
   public void testSimpleJs() throws Exception {
     checkJs(
         "{ var x = y; }",
-        "{" + weldSetOuters("x", weldReadOuters("y")) + "}");
+        "{" + DefaultCajaRewriterTest.weldSetOuters("x", DefaultCajaRewriterTest.weldReadOuters("y")) + "}");
   }
   
   private void checkJs(String original, String cajoled) throws IOException, ParseException {
-      httpService.setTest(original, "text/javascript");
-      String localTestServer = "http://localhost:8887/cajaservice";
-      String fetchUrl = "http://localhost:18887/cajaservicetest";
-      String mimeType = "text/javascript";
-      
-      String request = localTestServer 
-          + "?url=" + URLEncoder.encode(fetchUrl,"UTF-8")
-          + "&mime-type=" + URLEncoder.encode(mimeType, "UTF-8");
-      System.out.println(request);
-      String response = getTextRequest(request);
-      ParseTreeNodes.deepEquals(js(fromString(response)), js(fromString(cajoled)));
+    httpService.setTest(original, "text/javascript", "UTF-8");
+    String localTestServer = "http://localhost:" + CAJOLING_SERVICE_PORT + "/cajaservice";
+    String fetchUrl = "http://localhost:" + HTTP_TEST_PORT + "/cajaservicetest";
+    String mimeType = "text/javascript";
+    
+    String request = localTestServer 
+        + "?url=" + URLEncoder.encode(fetchUrl,"UTF-8")
+        + "&mime-type=" + URLEncoder.encode(mimeType, "UTF-8");
+    String response = getTextRequest(request);
+    ParseTreeNodes.deepEquals(js(fromString(response)), js(fromString(cajoled)));
   }
 
   private String getTextRequest(String testServer) 
@@ -110,46 +123,11 @@ public class CajolingServiceTest extends CajaTestCase {
     URL serverUrl = new URL(testServer);
     HttpURLConnection connection = (HttpURLConnection)serverUrl.openConnection();
     InputStreamReader content = new InputStreamReader(connection.getInputStream());
-    StringBuffer request = new StringBuffer();
+    StringBuilder request = new StringBuilder();
     int in;
     while ((in = content.read()) != -1) { 
       request.append((char)in);
     }
     return request.toString();
   }
-  
-  // From DefaultCajaRewriterTest:
-  
-  /**
-   * Welds together a string representing the repeated pattern of expected test output for
-   * assigning to an outer variable.
-   *
-   * @author erights@gmail.com
-   */
-  private static String weldSetOuters(String varName, String expr) {
-    return
-        "(function() {" +
-        "  var x___ = (" + expr + ");" +
-        "  return (___OUTERS___." + varName + "_canSet___ ?" +
-        "      (___OUTERS___." + varName + " = x___) :" +
-        "      ___.setPub(___OUTERS___,'" + varName + "',x___));" +
-        "})()";
-  }
-
-  /**
-   * Welds together a string representing the repeated pattern of expected test output for
-   * reading an outer variable.
-   *
-   * @author erights@gmail.com
-   */
-  private static String weldReadOuters(String varName) {
-    return weldReadOuters(varName, true);
-  }
-
-  private static String weldReadOuters(String varName, boolean flag) {
-    return
-        "(___OUTERS___." + varName + "_canRead___ ?" +
-        "    ___OUTERS___." + varName + ":" +
-        "    ___.readPub(___OUTERS___, '" + varName + "'" + (flag ? ", true" : "") + "))";
-  }  
 }
