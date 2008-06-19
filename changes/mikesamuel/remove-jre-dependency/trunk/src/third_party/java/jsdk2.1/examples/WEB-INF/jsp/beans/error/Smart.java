e2f251d29421853cb1449bcfb2e77f834847e779
mikesamuel <mikesamuel@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-06-19 05:13:45 +0000

    Remove dependency on JRE by reworking opensocial.service to use the JSDK.

---
+++ changes/mikesamuel/remove-jre-dependency/trunk/src/third_party/java/jsdk2.1/examples/WEB-INF/jsp/beans/error/Smart.java
@@ -0,0 +1,17 @@
+package error;
+
+import java.io.*;
+import java.lang.*;
+
+public class Smart {
+
+  String name = "JSP";
+
+  public String getName () {
+	return name;
+  }	
+
+  public void setName (String name) {
+	this.name = name;
+  }	
+}
