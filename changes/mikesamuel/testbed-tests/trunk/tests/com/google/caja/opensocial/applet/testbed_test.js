19b7b4119572ad357ef8fdc20fe7fa3a505f0531
mikesamuel <mikesamuel@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-07-24 21:22:42 +0000

    Some tests for the caja testbed.

---
+++ changes/mikesamuel/testbed-tests/trunk/tests/com/google/caja/opensocial/applet/testbed_test.js
@@ -0,0 +1,112 @@
+jsunitRegister('testIndentAndWrapCode', function () {
+  assertEquals('<br>', indentAndWrapCode(''));
+  assertEquals('<div class="line-of-code">foo</div>', indentAndWrapCode('foo'));
+  assertEquals(
+      ''
+      + '<div class="indentedblock>'
+        + '<div class="line-of-code">'
+          + 'foo'
+        + '</div>'
+      + '</div>',
+      indentAndWrapCode('  foo'));
+  assertEquals(
+      ''
+      + '<div class="line-of-code">'
+        + 'foo'
+      + '</div>'
+      + '<div class="indentedblock>'
+        + '<div class="line-of-code">'
+          + 'bar'
+        + '</div>'
+      + '</div>',
+      indentAndWrapCode(
+          ''
+          + 'foo\n'
+          + '  bar'));
+  assertEquals(
+      ''
+      + '<div class="line-of-code">'
+        // don't break on spaces.
+        + 'function&nbsp;foo(<wbr>)<wbr>&nbsp;{<wbr>'
+      + '</div>'
+      + '<div class="indentedblock>'
+        + '<div class="line-of-code">'
+          + 'var&nbsp;bar&nbsp;=&nbsp;baz(<wbr>'
+        + '</div>'
+        + '<div class="indentedblock>'
+          + '<div class="line-of-code">'
+            + 'a,<wbr>&nbsp;b,<wbr>&nbsp;&quot;c,&nbsp;d&quot;)<wbr>;'
+          + '</div>'
+        + '</div>'
+        + '<div class="line-of-code">'
+          // ampersands excaped
+          + 'return&nbsp;useBar(<wbr>)<wbr>&nbsp;&amp;&amp;&nbsp;bar;'
+        + '</div>'
+      + '</div>'
+      + '<div class="line-of-code">'
+        + '}<wbr>'
+      + '</div>',
+      indentAndWrapCode(
+          ''
+          + 'function foo() {\n'
+          + '  var bar = baz(\n'
+          + '      a, b, "c, d");\n'
+          + '  return useBar() && bar;\n'
+          + '}'));
+});
+jsunitRegister('testIndentAndWrapCode', function () {
+  assertEquals('<br>', indentAndWrapCode(''));
+  assertEquals('<div class="line-of-code">foo</div>', indentAndWrapCode('foo'));
+  assertEquals(
+      ''
+      + '<div class="indentedblock">'
+        + '<div class="line-of-code">'
+          + 'foo'
+        + '</div>'
+      + '</div>',
+      indentAndWrapCode('  foo'));
+  assertEquals(
+      ''
+      + '<div class="line-of-code">'
+        + 'foo'
+      + '</div>'
+      + '<div class="indentedblock">'
+        + '<div class="line-of-code">'
+          + 'bar'
+        + '</div>'
+      + '</div>',
+      indentAndWrapCode(
+          ''
+          + 'foo\n'
+          + '  bar'));
+  assertEquals(
+      ''
+      + '<div class="line-of-code">'
+        // don't break on spaces.
+        + 'function\xA0foo(<wbr>)<wbr>\xA0{<wbr>'
+      + '</div>'
+      + '<div class="indentedblock">'
+        + '<div class="line-of-code">'
+          + 'var\xA0bar\xA0=\xA0baz(<wbr>'
+        + '</div>'
+        + '<div class="indentedblock">'
+          + '<div class="line-of-code">'
+            + 'a,<wbr>\xA0b,<wbr>\xA0&quot;c,\xA0d&quot;)<wbr>;'
+          + '</div>'
+        + '</div>'
+        + '<div class="line-of-code">'
+          // ampersands excaped
+          + 'return\xA0useBar(<wbr>)<wbr>\xA0&amp;&amp;\xA0bar;'
+        + '</div>'
+      + '</div>'
+      + '<div class="line-of-code">'
+        + '}<wbr>'
+      + '</div>',
+      indentAndWrapCode(
+          ''
+          + 'function foo() {\n'
+          + '  var bar = baz(\n'
+          + '      a, b, "c, d");\n'
+          + '  return useBar() && bar;\n'
+          + '}'));
+});
