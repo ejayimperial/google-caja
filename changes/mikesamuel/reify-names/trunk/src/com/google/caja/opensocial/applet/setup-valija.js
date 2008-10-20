7dd80c580254e1314894cabb580e55948d240b52
mikesamuel <mikesamuel@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-10-20 20:33:35 +0000

    Delete.

--- changes/mikesamuel/reify-names/trunk/src/com/google/caja/opensocial/applet/setup-valija.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  var imports = ___.getNewModuleHandler().getImports();
-  imports.loader = {provide:___.simpleFunc(function(v){valijaMaker = v;})};
-  imports.outers = imports;
-})();
