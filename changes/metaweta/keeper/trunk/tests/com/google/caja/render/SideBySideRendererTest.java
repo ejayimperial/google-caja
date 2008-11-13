d0faad28f5b2a8909256653c5d1e50876fdc730f
metaweta <metaweta@360991c0-9f3b-0410-ba87-8b362a00edf4>
2008-11-13 19:08:04 +0000

    Adds generic read, set, call, delete, and enum handlers to TameNode to support setting properties on DOM nodes. The handlers first check if some other handler exists for <name>; if not, it adds the property to the "properties___" object on the underlying DOM node.
    Also fixes a bug in parsing style attributes.

--- changes/metaweta/keeper/trunk/tests/com/google/caja/render/SideBySideRendererTest.java
+++ changes/metaweta/keeper/trunk/tests/com/google/caja/render/SideBySideRendererTest.java
@@ -14,79 +14,27 @@
 
 package com.google.caja.render;
 
-import com.google.caja.lexer.FilePosition;
+import com.google.caja.lexer.TokenConsumer;
 import com.google.caja.lexer.InputSource;
 import com.google.caja.reporting.MessageContext;
-import com.google.caja.util.TestUtil;
+import com.google.caja.util.Callback;
 
-import junit.framework.TestCase;
-
-import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.IOException;
 
 /**
- * @author mikesamuel@gmail.com
+ * @author ihab.awad@gmail.com
  */
-public class SideBySideRendererTest extends TestCase {
+public class SideBySideRendererTest extends OrigSourceRendererTestCase {
   public void testRendering() throws Exception {
     runTest(
-        "side-by-side-golden.js", "rewritten-input.txt",
-        "test-input1.js", "test-input2.html", "test-input3.css");
+        "sbs-golden.js", "sbs-rewritten-tokens.txt",
+        "sbs-test-input1.js", "sbs-test-input2.html", "sbs-test-input3.css");
   }
 
-  private void runTest(String goldenFile, String rewrittenFile,
-                       String... originalSourceFiles) throws Exception {
-    final MessageContext mc = new MessageContext();
-
-    Map<InputSource, String> originalSrcs = new HashMap<InputSource, String>();
-    for (String originalSourceFile : originalSourceFiles) {
-      InputSource is = new InputSource(
-          TestUtil.getResource(getClass(), originalSourceFile));
-      originalSrcs.put(
-          is, TestUtil.readResource(getClass(), originalSourceFile));
-    }
-    mc.inputSources = originalSrcs.keySet();
-
-    StringBuilder actual = new StringBuilder();
-    SideBySideRenderer r = new TabularSideBySideRenderer(
-        originalSrcs, mc, actual, null);
-    for (String line
-         : TestUtil.readResource(getClass(), rewrittenFile).split("\n")) {
-      if (line.startsWith("#")) {
-        line = line.substring(1).trim();
-        if ("<null>".equals(line)) {
-          r.mark(null);
-        } else {
-          Matcher m = Pattern.compile(
-              "(.*):(\\d+)\\+(\\d+)-(?:(\\d+)\\+)?(\\d+)$")
-              .matcher(line);
-          if (!m.matches()) { throw new RuntimeException(line); }
-          String basename = m.group(1);
-          int sln = Integer.parseInt(m.group(2));
-          int slc = Integer.parseInt(m.group(3));
-          String g4 = m.group(4);
-          int eln = g4 != null ? Integer.parseInt(g4) : sln;
-          int elc = Integer.parseInt(m.group(5));
-          InputSource src = null;
-          for (InputSource candidate : originalSrcs.keySet()) {
-            if (candidate.getUri().getPath().endsWith(basename)) {
-              src = candidate;
-            }
-          }
-          if (src == null) { throw new RuntimeException(basename); }
-          r.mark(FilePosition.instance(
-              src, sln, sln, slc, eln, eln, elc));
-        }
-      } else {
-        r.consume(line);
-      }
-    }
-    r.noMoreTokens();
-
-    assertEquals(
-        TestUtil.readResource(getClass(), goldenFile).replace("@", "\uFFFD"),
-        actual.toString());
+  protected TokenConsumer createRenderer(
+      Map<InputSource, ? extends CharSequence> originalSource,
+      MessageContext mc, Appendable out, Callback<IOException> exHandler) {
+    return new TabularSideBySideRenderer(originalSource, mc, out, exHandler);
   }
 }
